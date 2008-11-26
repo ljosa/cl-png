a5919243805eb1b5f38b88b4435048fd89d46103
Vebjorn Ljosa <vebjorn@ljosa.com>
2008-11-26 22:38:44 +0000

    Keep the stream from being GCed before we close it.

--- compat.lisp
+++ compat.lisp
@@ -36,9 +36,12 @@
   (file :pointer))
 
 (defmacro with-file ((var stream &optional (mode "rb")) &body body)
-  `(let ((,var (fdopen (stream-fd ,stream) ,mode)))
-     (unwind-protect (progn ,@body)
-       (fclose ,var))))
+  (let ((stream (gensym "STREAM")))
+    ; Keep the stream from being GCed before we close it.
+    `(let ((,stream-var ,stream)) 
+       (let ((,var (fdopen (stream-fd ,stream) ,mode)))
+	 (unwind-protect (progn ,@body)
+	   (fclose ,var))))))
 
 
 #|
